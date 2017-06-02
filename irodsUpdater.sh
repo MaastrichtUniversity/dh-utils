16d5c86c4a4eca02ddc86a8187423c16f8682cdd
Paul van Schayck <polleke@gmail.com>
2017-06-02 14:48:42 +0200

    Add ability to dry run changes. Add README

--- irodsUpdater.sh
+++ irodsUpdater.sh
@@ -25,8 +25,6 @@ do
         # create backup path on local disk
         mkdir -p processing/$p
 
-        # TODO: We should place metadata.xml version information somewhere, as iRODS metadata or in the XML itself
-
         # download metadata.xml from irods
         iget -f $base/$p/metadata.xml processing/$p/metadata.xml > /dev/null 2>&1
 
@@ -44,14 +42,17 @@ do
         # parse xml in python and change xml tag
         python migrations/$version/migrate.py processing/$p/metadata.xml processing/$p/metadata.new.xml
 
-        # Open project collection for writing
-        irule "openProjectCollection('$project', '$collection')" null null
+        # Only put results back when --commit is the argument
+        if [[ $1 == "--commit" ]]; then
+            # Open project collection for writing
+            irule "openProjectCollection('$project', '$collection')" null null
 
-        # Update file in irods
-        iput -f processing/$p/metadata.new.xml $base/$p/metadata.xml
+            # Update file in irods
+            iput -f processing/$p/metadata.new.xml $base/$p/metadata.xml
 
-        # Close project collection for writing
-        irule "closeProjectCollection('$project', '$collection')" null null
+            # Close project collection for writing
+            irule "closeProjectCollection('$project', '$collection')" null null
+        fi;
 
         # Stop breaking on any error from here on
         set +e
