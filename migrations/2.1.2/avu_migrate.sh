5aad8c64e37dfbc931323989c1f74afd4460e7d8
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2018-03-28 16:45:31 +0200

    Added setting the AVU for 'numFiles' (RITDEV-387 and RITDEV-405)

--- migrations/2.1.2/avu_migrate.sh
+++ migrations/2.1.2/avu_migrate.sh
@@ -2,9 +2,12 @@
 path="/nlmumc/projects/$1"
 
 size=$(irule "calcCollectionSize('$path', 'B', 'none', *result); writeLine('stdout', *result);" null ruleExecOut)
+numFiles=$(irule "calcCollectionFiles('$path', *result); writeLine('stdout', *result);" null ruleExecOut)
 
 echo "Setting dcat:byteSize to $size"
+echo "Setting numFiles to $numFiles"
 
 if [[ $2 == "--commit" ]]; then
     imeta set -C $path 'dcat:byteSize' $size
+    imeta set -C $path 'numFiles' $numFiles
 fi
