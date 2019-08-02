ab47aded27922ac874cf066745e367f08fc89a4c
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2019-08-02 14:49:31 +0200

    Use iquest to find projects with the AVU present & use imeta rm with wildcards for value (DHO-31)

--- irods/irodsRemoveAvuFromProjects/irodsRemoveAvuFromProjects.sh
+++ irods/irodsRemoveAvuFromProjects/irodsRemoveAvuFromProjects.sh
@@ -2,21 +2,23 @@
 
 # BEFORE YOU BEGIN: Set the correct resource values
 AvuName='NCIT:C88193'
-projectsRoot='/nlmumc/projects/'
+projectsRoot='/nlmumc/projects'
 
 
 if [[ $1 != "--commit" ]]; then
     echo "---- RUNNING SCRIPT IN DRY RUN ----"
 fi;
 
-# Retrieve list of all projects
-projects=( $(ils ${projectsRoot} | awk '{print $2}'))
+# Retrieve list of all projects with AvuName present
+projects=( $(iquest "select COLL_NAME where COLL_PARENT_NAME like '${projectsRoot}' and META_COLL_ATTR_NAME like '${AvuName}'" | awk '{print $3}' | grep /nlmumc ))
 
 # Loop over projects to remove the AVU
 for i in "${projects[@]}"
 do
     echo "Removing AVU ${AvuName} from project $i"
     if [[ $1 == "--commit" ]]; then
-        imeta rm -C $i ${AvuName}
+        # imeta rmw -C Name AttName AttValue
+        # using % wildcard as AttValue
+        imeta rmw -C "${i}" "${AvuName}" "%"
     fi;
 done
