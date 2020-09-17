7abca044c651796647a297d7a43ce072ebb40cc0
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2020-09-17 14:42:21 +0200

    added the first version of the user preparation script for SRAM [DHS-808,DHS-813]

---
+++ irods/irodsPrepareUsersForSRAM/irodsPrepareUsersForSRAM.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+if [[ $1 != "--commit" ]]; then
+    echo "---- RUNNING SCRIPT IN DRY RUN ----"
+fi;
+
+# Retrieve list rodsusers
+users=( $(iquest "select USER_NAME where USER_TYPE = 'rodsuser'" | grep USER | awk '{print $3}'))
+
+# Loop over users to add the correct AVU
+for i in "${users[@]}"
+do
+    if [[ $i == service-* ]] ; then
+        echo "Adding AVU ldapSync false to service user $i"
+        if [[ $1 == "--commit" ]]; then
+           imeta add -u "${i}" "ldapSync" "false"
+        fi;
+    else
+      echo "Adding AVU pendingSramInvite user $i"
+        if [[ $1 == "--commit" ]]; then
+           imeta add -u "${i}" "pendingSramInvite" " "
+        fi;
+
+    fi;
+done
