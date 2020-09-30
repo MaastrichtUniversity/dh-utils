648ccad82dd0d2506a1c413d11e5c15206574e93
iRODS Administrator <irods@fhml-srv038.unimaas.nl>
2020-09-30 12:50:53 +0200

    Added correct command for unpaged iquest results [DHS-813]

--- irods/irodsPrepareUsersForSRAM/irodsPrepareUsersForSRAM.sh
+++ irods/irodsPrepareUsersForSRAM/irodsPrepareUsersForSRAM.sh
@@ -5,9 +5,7 @@ if [[ $1 != "--commit" ]]; then
 fi;
 
 # Retrieve list rodsusers
-# Yes was added because iquests pauses output when there are too many
-# One person gets replaced by an equal sign. That user was manually updated to have the pendingSramInvite AVU
-users=( $(yes | iquest "select USER_NAME where USER_TYPE = 'rodsuser'" | grep USER | awk '{print $3}'))
+users=( $(iquest --no-page "select USER_NAME where USER_TYPE = 'rodsuser'" | grep USER | awk '{print $3}'))
 
 # Loop over users to add the correct AVU
 for i in "${users[@]}"
@@ -18,12 +16,9 @@ do
            imeta set -u "${i}" "ldapSync" "false"
         fi;
     else
-       # The equal sign gets added in the output because of the 'yes' 
-        if [[ $i != "=" ]]; then
-	    echo "Adding AVU pendingSramInvite user $i"
-            if [[ $1 == "--commit" ]]; then
-               imeta set -u "${i}" "pendingSramInvite" "true"
-            fi;
-	fi;
+	echo "Adding AVU pendingSramInvite user $i"
+        if [[ $1 == "--commit" ]]; then
+           imeta set -u "${i}" "pendingSramInvite" "true"
+        fi;
     fi;
 done
