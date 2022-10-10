5bd57fd0d109e4231ace84803e4f74c46385e700
lwinckers <laurent_2207@live.nl>
2022-10-10 11:44:07 +0200

    Add archival operation and finiliza script [DHDO-469]

--- deployment/irodsDeploymentTests.py
+++ deployment/irodsDeploymentTests.py
@@ -25,6 +25,7 @@ def parse_args():
     parser.add_argument("-d", "--dest", default=None, action='store',
                         required=False, type=str, help="Destination path to locally store file from iRODS.")
     parser.add_argument("-o", "--overwrite", required=False, action='store_true', help="Overwrite files if they exist.")
+    parser.add_argument("-a", "--archive-file", required=False, action='store_true', help="Archive file to tape.")
 
     return parser.parse_args()
 
@@ -93,6 +94,7 @@ def irods_session(env_file=None):
     return session
 
 
+# Returns all 'replication' resources available.
 def get_resources(session):
     repl_coor_rescs = session.query(Resource).filter(Resource.type == 'replication')
     repl_coor_rescs_names = [repl_resc[Resource.name] for repl_resc in repl_coor_rescs]
@@ -101,7 +103,8 @@ def get_resources(session):
     return repl_coor_rescs_names
 
 
-# Slightly modified, based on "check_irods_resources_availability.py" Nagios check
+# Slightly modified, based on "check_irods_resources_availability.py" Nagios check.
+# Checks all resources.
 def check_resources(session, exclusions):
     resources = [iRODSResource(session.resources, query_result) for query_result in session.query(Resource).all()]
     for resource in resources:
@@ -119,50 +122,76 @@ def check_resources(session, exclusions):
             log.warning(f"Resource '{resource.name}' has status '{resource.status}'")
 
 
+# Make sure path exists, in this case the home folder of the user.
 def check_path(session):
     path = f"/nlmumc/home/{session.username}"
     if not session.collections.exists(path):
-        log.error(f"The path {path} does not exist. Make sure it exists before proceeding!")
+        log.error(f"The path {path}/ does not exist. Make sure it exists before proceeding!")
         log.error(f"Exiting...")
         sys.exit(1)
 
 
+# Check if file(name) already exist and ask if it is okay to overwrite it.
+# If overwrite flag is passed it will overwrite if file(name) already exists.
 def check_file(session, resources, name, overwrite):
     path = f"/nlmumc/home/{session.username}"
-    file_path = f"/nlmumc/home/{session.username}/{name}"
+    file_path = f"{path}/{name}"
     for resource in resources:
         if session.data_objects.exists(str(file_path+"_"+resource)) and overwrite is False:
-            yes_no = query_yes_no(f"The filename '{name}_{resource}' already exists at {path}, resource {resource}. Do you want to overwrite the file?")
+            yes_no = query_yes_no(f"The filename '{name}_{resource}' already exists at {path}/, resource {resource}. Do you want to overwrite the file?")
             if yes_no == "no":
                 log.info(f"Exiting... Please retry using a different filename.")
                 sys.exit(1)
+        elif session.data_objects.exists(str(file_path+"_"+resource)) and overwrite is True:
+            log.info(f"Overwrite flag passed. Will overwrite file '{name}_{resource}' at '{path}/'")
 
 
+# Put operation of file to all available 'replication' resources.
 def put_file(session, resources, name, source_file):
-    path = f"/nlmumc/home/{session.username}/{name}"
+    path = f"/nlmumc/home/{session.username}"
+    file_path = f"{path}/{name}"
     try:
         for resource in resources:
-            log.info(f"Putting file '{name}_{resource}' to '{path}', resource '{resource}'")
-            session.data_objects.put(source_file, str(path+"_"+resource), destRescName=resource)
-            # session.data_objects.replicate(path, resource="arcRescSURF01")
+            log.info(f"Putting file '{name}_{resource}' to '{path}/', resource '{resource}'")
+            session.data_objects.put(source_file, str(file_path+"_"+resource), destRescName=resource)
             log.info(f"Put operation to '{resource}' successful!")
     except (
             exception.DataObjectDoesNotExist,
             exception.SYS_FILE_DESC_OUT_OF_RANGE,
             exception.UNIX_FILE_CREATE_ERR,
-            KeyError,
+            KeyError
     ):
         log.error(f"Error: during put operation. Exiting...")
         sys.exit(1)
 
 
+# Archival of file to tape by replicating the file with destination resource 'arcRescSURF01'.
+def archive_file(session, resources, name, archive_file):
+    path = f"/nlmumc/home/{session.username}"
+    file_path = f"{path}/{name}"
+    if archive_file is True:
+        try:
+            for resource in resources:
+                log.info(f"Archiving file '{name}_{resource}' from '{path}/' to 'arcRescSURF01'")
+                session.data_objects.replicate(str(file_path+"_"+resource), resource="arcRescSURF01")
+                log.info(f"Archival to 'arcRescSURF01' successful!")
+        except(
+                exception.DataObjectDoesNotExist,
+                KeyError
+        ):
+            log.error(f"Error during archive operation. Exiting...")
+            sys.exit(1)
+
+
+# Get operation of file
 def get_file(session, resources, name, dest):
-    path = f"/nlmumc/home/{session.username}/{name}"
+    path = f"/nlmumc/home/{session.username}"
+    file_path = f"{path}/{name}"
     try:
         for resource in resources:
-            log.info(f"Getting file '{name}_{resource}' from '{path}', resource '{resource}")
+            log.info(f"Getting file '{name}_{resource}' from '{path}/', resource '{resource}")
             log.info(f"Destination: '{dest}'")
-            session.data_objects.get(str(path+"_"+resource), dest)
+            session.data_objects.get(str(file_path+"_"+resource), dest)
             log.info(f"Get operation successful!")
     except (
             exception.OVERWRITE_WITHOUT_FORCE_FLAG,
@@ -172,16 +201,18 @@ def get_file(session, resources, name, dest):
         return 1
 
 
+# Removal of all files based on filename, with unlink. This also removes all replicates.
 def remove_file(session, resources, name):
-    path = f"/nlmumc/home/{session.username}/{name}"
+    path = f"/nlmumc/home/{session.username}"
+    file_path = f"{path}/{name}"
     try:
         for resource in resources:
-            log.info(f"Removing file '{name}_{resource}' from '{path}', resource '{resource}'")
-            session.data_objects.get(str(path+"_"+resource)).unlink(force=True)
+            log.info(f"Removing file '{name}_{resource}' from '{path}/', resource '{resource}'")
+            session.data_objects.get(str(file_path+"_"+resource)).unlink(force=True)
             log.info(f"Removal from resource: '{resource}' successful!")
     except (
             exception.DataObjectDoesNotExist,
-            KeyError,
+            KeyError
     ):
         log.error(f"Error: during remove operation. Exiting...")
         sys.exit(1)
@@ -191,17 +222,18 @@ def main():
     args = parse_args()
     print(args)
     with irods_session(args.env_file) as session:
-        resources = get_resources(session)
-        print()
-
         log.info("START: checking resource availability...")
         check_resources(session, args.exclusions)
+        print()
+
+        resources = get_resources(session)
         print()
 
         log.info("START: putting, getting and removing file...")
         check_path(session)
         check_file(session, resources, args.name, args.overwrite)
         put_file(session, resources, args.name, args.source_file)
+        archive_file(session, resources, args.name, args.archive_file)
         if get_file(session, resources, args.name, args.dest) == 1:
             remove_file(session, resources, args.name)
         else:
